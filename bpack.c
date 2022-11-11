6af3247d84f6c6b300b5925be6509567cec1267d
Tyler Pease <40471000+SirTarragon@users.noreply.github.com>
2022-11-10 19:15:06 -0500

    to handle extreme file sizes

--- bpack.c
+++ bpack.c
@@ -7,14 +7,17 @@
 #include <stdlib.h>
 #include <string.h>
 
-void encore(unsigned char *, const unsigned char *, const int, const int, const int);
+#define MAX_ACCEPT_SIZE 200000000
+
+void encoreBuffered(unsigned char *, const unsigned char *, const int, const int, const int);
+void encoreDirect(FILE *, FILE *, const int, const int, const int);
 void applyMask(unsigned long long *, const int, const int);
 
 int main(int argc, char *argv[]) {
     FILE *inputFile, *outputFile;
     char *inputFileName, *outputFileName, *mode;
     unsigned char *buffer, *o_buffer;
-    int fileSize, newSize;
+    int fileSize, newSize, m_val;
 
     // need to make sure that input and output file names are provided
     if (argc != 4) {
@@ -50,31 +53,39 @@ int main(int argc, char *argv[]) {
     fileSize = ftell(inputFile);
     rewind(inputFile);
 
-    buffer = (unsigned char *)malloc(sizeof(unsigned char) * fileSize);
-    if (buffer == NULL) {
-        printf("Error: Cannot allocate memory for input buffer\n");
-        exit(1);
+    m_val = strcmp(mode, "-c");
+
+    if(fileSize > MAX_ACCEPT_SIZE) { // if the file is less than 200MB, use buffered method
+        // this prevents issues with larger files, but is slower
+        (m_val == 0) ? encoreDirect(outputFile, inputFile, fileSize, 8, 7): 
+                       encoreDirect(outputFile, inputFile, fileSize, 7, 8);
+    } else { // we can use the seemingly faster method
+        buffer = (unsigned char *)malloc(sizeof(unsigned char) * fileSize);
+        if (buffer == NULL) {
+            printf("Error: Cannot allocate memory for input buffer\n");
+            exit(1);
+        }
+
+        // going to need another buffer to store the decompressed file
+        newSize = (m_val == 0) ? (fileSize * 7)/8 : (fileSize * 8)/7;
+        o_buffer = (unsigned char *)malloc(sizeof(unsigned char) * newSize);
+        if (o_buffer == NULL) { // verify that the memory was allocated
+            printf("Error: Cannot allocate memory for output buffer\n");
+            exit(1);
+        }
+
+        // read, influence the bits, and write
+        fread(buffer, sizeof(unsigned char), fileSize, inputFile);
+        (m_val == 0) ? encoreBuffered(o_buffer, buffer, fileSize, 8, 7):
+                       encoreBuffered(o_buffer, buffer, fileSize, 7, 8);
+        fwrite(o_buffer, sizeof(unsigned char), newSize, outputFile);
+
+        // free the memory
+        free(buffer); free(o_buffer);
     }
 
-    // going to need another buffer to store the decompressed file
-    newSize = (strcmp(mode, "-c") == 0) ? (fileSize * 7)/8 : (fileSize * 8)/7;
-    o_buffer = (unsigned char *)malloc(sizeof(unsigned char) * newSize);
-    if (o_buffer == NULL) { // verify that the memory was allocated
-        printf("Error: Cannot allocate memory for output buffer\n");
-        exit(1);
-    }
-
-    fread(buffer, 1, fileSize, inputFile);
-
-    (strcmp(mode, "-c") == 0) ? encore(o_buffer, buffer, fileSize, 8, 7) : 
-                                encore(o_buffer, buffer, fileSize, 7, 8);
-
-    // write the decompressed file to the output file
-    fwrite(o_buffer, 1, newSize, outputFile);
-
-    // close the files and free the memory
+    // close the files
     fclose(inputFile); fclose(outputFile);
-    free(buffer); free(o_buffer);
 
     return 0;
 }
@@ -86,7 +97,7 @@ void applyMask(unsigned long long * buffer, const int c, const int o) { // goes 
     ((*buffer >> (c*6)) & 0x7F) << (o*6) | ((*buffer >> (c*7)) & 0x7F) << (o*7);
 }
 
-void encore(unsigned char *output, const unsigned char *input, const int size, const int c, const int o) {
+void encoreBuffered(unsigned char *output, const unsigned char *input, const int size, const int c, const int o) {
     // c is the number of bits per byte for the input buffer, o is the same for the output
     // 8 bits for compression or 7 bits for decompression are the expected values of c, o will be the opposite to c
     for(int i = size, memsize = (i > c) ? c : i; i > 0; i -= memsize, memsize = (i > c) ? c : i) {
@@ -96,5 +107,16 @@ void encore(unsigned char *output, const unsigned char *input, const int size, c
         applyMask(&buffer, c, o);
         memcpy(output, &buffer, o * sizeof(unsigned char));
         output += o;
+    }
+}
+
+void encoreDirect(FILE *output, FILE *input, const int size, const int c, const int o) {
+    // same thing as encoreBuffered, but essentially streams the data directly from the input file to the output file
+    // it can be slower than the buffered method, but it is more memory efficient
+    for(int i = size, memsize = (i > c) ? c : i; i > 0; i -= memsize, memsize = (i > c) ? c : i) {
+        unsigned long long buffer = 0;
+        fread(&buffer, 1, memsize, input);
+        applyMask(&buffer, c, o);
+        fwrite(&buffer, 1, o, output);
     }
 }
